770ae01053d232496ccfc844c85a3f0c4c8a7d04
Lorenzo Caldera <115328112+LorenzoCaldera@users.noreply.github.com>
2023-02-21 00:19:53 -0300

    color waves

--- src/components/about-me.jsx
+++ src/components/about-me.jsx
@@ -3,7 +3,9 @@ import React from "react"
 function AboutMe() {
     return (
         <section className="main__content">
+            <div className="horizontal__line"></div>
             <h1>Lorenzo Caldera Web Developer Indie</h1>
+            <div className="horizontal__line"></div>
         </section>
     )
 }
