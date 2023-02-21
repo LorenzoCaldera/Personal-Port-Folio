ca8d7dad4e4d4cc672ee453a022e045807c62062
Lorenzo Caldera <115328112+LorenzoCaldera@users.noreply.github.com>
2023-02-21 14:45:07 -0300

    color waves added && remove header.css
    header.css had only 15 lines

--- src/components/about-me.jsx
+++ src/components/about-me.jsx
@@ -3,12 +3,17 @@ import React from "react"
 function AboutMe() {
     return (
         <section className="main__content">
-            <div className="waves">
+            <div className="blue__waves waves">
                 <div></div>
                 <div></div>
                 <div></div>
             </div>
             <h1>Lorenzo Caldera Web Developer Indie</h1>
+            <div className="red__waves waves">
+                <div></div>
+                <div></div>
+                <div></div>
+            </div>
         </section>
     )
 }
