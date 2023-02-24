ba9dcf22b67bc069889818c54928b2b298a1a2d3
Lorenzo Caldera <115328112+LorenzoCaldera@users.noreply.github.com>
2023-02-24 19:54:51 -0300

    Update projects.jsx
    description of todo-list

--- src/components/projects.jsx
+++ src/components/projects.jsx
@@ -6,7 +6,7 @@ function Projects () {
             <h1>PROJECTS</h1>
             <div>
                 <h2>TODO-LIST: </h2>
-                <p>"Todo-list" is a webs sites where bla bla bla </p>
+                <p>This application is developed in React and has several useful features, including a messaging chat, a to-do list, and a calendar. The task manager allows users to add, delete, and complete tasks, as well as a system for expiration. The calendar provides the option to create reminders and select repetition cycles. The chat enables users to communicate with other members of their work team through groups or private chats.</p>
                 <img />
             </div>
         </section>
