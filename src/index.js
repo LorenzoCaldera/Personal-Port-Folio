fcb183ca53a0c1107d34806eaeb6854ded7c0aa1
Lorenzo Caldera <115328112+LorenzoCaldera@users.noreply.github.com>
2023-02-27 13:38:55 -0300

    mousetracker added && css clean code

--- src/index.js
+++ src/index.js
@@ -4,3 +4,24 @@ import App from './App';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(<App />);
+
+let mouseTracker = document.getElementsByClassName("mouse-tracker"),
+    scroll
+
+window.onscroll = () => {
+    scroll = window.scrollY
+}
+
+document.body.onpointermove = e => {
+    const { clientX, clientY } = e
+
+    mouseTracker[0].animate({
+        top: `${clientY + scroll}px`,
+        left: `${clientX}px`
+    }, {duration: 3000, fill: "forwards"})
+    
+    mouseTracker[1].animate({
+        top: `${clientY + scroll}px`,
+        left: `${clientX}px`
+    }, {duration: 1000, fill: "forwards"})
+}
