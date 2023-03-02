beb3ad455753212d4a14e9b74969b379adbcecc6
Lorenzo Caldera <115328112+LorenzoCaldera@users.noreply.github.com>
2023-03-02 18:29:11 -0300

    Update side-bar.jsx
    path WIP

--- src/components/side-bar.jsx
+++ src/components/side-bar.jsx
@@ -4,14 +4,12 @@ function SideBar () {
     return (
         <React.Fragment>
             <svg stroke="var(--c-black)" height="100" width="100" viewBox="0 0 100 100">
-                <line
-                    x1="20" x2="80" y1="40" y2="40"
-                    strokeWidth="10"
-                ></line>
-                <line
-                    x1="20" x2="60" y1="60" y2="60"
-                    strokeWidth="10"
-                ></line>
+                <path
+                    d="m 80 20 l -60 60 h 0 v -30 h 40 m -40 -30 l 60 60 h 0 v -40 h -60"
+                    fill="transparent"
+                    strokeWidth="5"
+                    strokeLinejoin="round">
+                </path>
             </svg>
             <span>
 
