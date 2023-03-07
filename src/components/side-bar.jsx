a6b3717d9f4ef9bceb23b36f83e4eb8af9712204
Lorenzo Caldera <115328112+LorenzoCaldera@users.noreply.github.com>
2023-03-06 23:57:08 -0300

    adding the css && fixing class => className

--- src/components/side-bar.jsx
+++ src/components/side-bar.jsx
@@ -4,11 +4,12 @@ function SideBar () {
     const [sideBarState, setSideBarState] = React.useState(false)
 
     const handlerToggleSideBar = () => {
+        console.log(sideBarState)
         setSideBarState(!setSideBarState)
     }
 
     return (
-        <div id="side-bar" class={`${sideBarState ? "open" : "close"}`}>
+        <div id="side-bar" className={`${sideBarState ? "open" : "close"}`}>
             <svg className="menu" viewBox="0 0 100 100" width="80" onClick={handlerToggleSideBar}>
                 <path
                     d="m 30,33 h 40 c 13.100415,0 14.380204,31.80258 6.899646,33.421777 -24.612039,5.327373 9.016154,-52.337577 -12.75751,-30.563913 l -28.284272,28.284272"
