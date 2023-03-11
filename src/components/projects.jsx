349dab69520796d30f5eb1d44f0b6210fceffbf7
Lorenzo Caldera <115328112+LorenzoCaldera@users.noreply.github.com>
2023-03-10 23:51:31 -0300

    Update projects.jsx
    I nested all code-btn inside of an anchor, in that way you can press the button everywhere and at any time and it still working

--- src/components/projects.jsx
+++ src/components/projects.jsx
@@ -11,17 +11,19 @@ function Projects () {
                     <p>This application is developed in React and has several useful features, including a messaging chat, a to-do list, and a calendar.</p>
                     <div className="projects__buttons">
                         <button style={{cursor: "not-allowed"}} className="visit-btn btn__hover-eff">See live</button>
-                        <button className="code-btn btn__color-eff">
-                            <span className="colors-layer">
-                                <span className="first-layer"></span>
-                                <span className="second-layer"></span>
-                                <span className="third-layer"></span>
-                            </span>
-                            <span className="text-move">
-                                <a href="https://github.com/LorenzoCaldera/Task-List" className="main-text">Source code</a>
-                                <a href="https://github.com/LorenzoCaldera/Task-List" className="secon-text">Source code</a>
-                            </span>
-                        </button>
+                        <a href="https://github.com/LorenzoCaldera/Task-List">
+                            <button className="code-btn btn__color-eff">
+                                <span className="colors-layer">
+                                    <span className="first-layer"></span>
+                                    <span className="second-layer"></span>
+                                    <span className="third-layer"></span>
+                                </span>
+                                <span className="text-move">
+                                    <a className="main-text">Source code</a>
+                                    <a className="secon-text">Source code</a>
+                                </span>
+                            </button>
+                        </a>
                     </div>
                 </div>
                 <img src={ToDoList} />
