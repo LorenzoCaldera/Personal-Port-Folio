c69fbb75bb24e7717258c1a616f88ac131d4f7dd
Lorenzo Caldera <115328112+LorenzoCaldera@users.noreply.github.com>
2023-03-15 23:41:59 -0300

    Update language.jsx
    added the languages options and a function that console.log the language you choose

--- src/components/language.jsx
+++ src/components/language.jsx
@@ -1,8 +1,20 @@
 import React from "react"
 
 function ChooseLanguage() {
+    const changeLanguage = (e) => {
+        console.log("you choose: " + e.target.innerHTML)
+    }
+
     return (
-        <div></div>
+        <span id="languages">
+            <ul>
+                <li onClick={changeLanguage}>English</li>
+                <li onClick={changeLanguage}>Español</li>
+                <li onClick={changeLanguage}>Italiano</li>
+                <li onClick={changeLanguage}>Português</li>
+                <li onClick={changeLanguage}>Frances</li>
+            </ul>
+        </span>
     )
 }
 
