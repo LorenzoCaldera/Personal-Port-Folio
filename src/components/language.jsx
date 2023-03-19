3ee1f88fc047474736cd7e24837b665edd1773df
Lorenzo Caldera <115328112+LorenzoCaldera@users.noreply.github.com>
2023-03-19 20:29:21 -0300

    changin the html.lang when you click you r language

--- src/components/language.jsx
+++ src/components/language.jsx
@@ -2,17 +2,18 @@ import React from "react"
 
 function ChooseLanguage() {
     const changeLanguage = (e) => {
-        console.log("you choose: " + e.target.innerHTML)
+        console.log("you choose: " + document.querySelector("html").lang)
+        document.querySelector("html").lang = e.target.dataset.lang
     }
 
     return (
         <span id="languages">
             <ul>
-                <li onClick={changeLanguage}>English</li>
-                <li onClick={changeLanguage}>Español</li>
-                <li onClick={changeLanguage}>Italiano</li>
-                <li onClick={changeLanguage}>Português</li>
-                <li onClick={changeLanguage}>Frances</li>
+                <li data-lang="en" onClick={changeLanguage}>English</li>
+                <li data-lang="es" onClick={changeLanguage}>Español</li>
+                <li data-lang="it" onClick={changeLanguage}>Italiano</li>
+                <li data-lang="pt" onClick={changeLanguage}>Português</li>
+                <li data-lang="fr" onClick={changeLanguage}>Frances</li>
             </ul>
         </span>
     )
