b2f1fb2fcb9fe20f39fb3911c7471646a37b8fef
Lorenzo Caldera <115328112+LorenzoCaldera@users.noreply.github.com>
2023-03-21 00:02:22 -0300

    removing interpolation

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App'
+// import App from './App'
 
 import { createRoot } from 'react-dom/client';
 import i18n from "i18next";
@@ -18,10 +18,6 @@ i18n
     },
     lng: "en",
     fallbackLng: "en",
-
-    interpolation: {
-      escapeValue: false
-    }
   });
 
 function App() {
