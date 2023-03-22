cf99b3d61b3b40f759d0852e3cbeb85092bad2a4
Lorenzo Caldera <115328112+LorenzoCaldera@users.noreply.github.com>
2023-03-21 23:19:21 -0300

    Update about-me.jsx
    About me now can be translated

--- src/components/about-me.jsx
+++ src/components/about-me.jsx
@@ -1,9 +1,13 @@
 import React from "react"
 import scrollEff from "../assets/scroll-eff"
 
+import { useTranslation } from "react-i18next"
+
 function AboutMe() {
     const [welcomeClass, setWelcomeClass] = React.useState("show")
     const welcomeRef = React.useRef()
+
+    const { t } = useTranslation()
 
     const [parragraphClass, setparragraphClass] = React.useState("show")
     const parragraphRef = React.useRef()
@@ -50,10 +54,10 @@ function AboutMe() {
                 </div>
             </header>
             <div ref={parragraphRef} className={parragraphClass} style={{display: "flex", flexDirection: "column", alignItems: "center"}}>
-                <p> Hi there! I'm a 17-year-old web developer passionate about programming and web design. My main focus is on creating functional web pages with an attractive design and clean, readable code.</p>
-                <p> As a full-stack developer, I have skills in both front-end and back-end development. This means that I can create the structure and functionality of the web page, as well as design the user interface and user experience. Additionally, I can configure servers to make the web pages available online for everyone.</p>
-                <p> I strive to stay up-to-date with the latest trends and technologies in web development to ensure that my web pages are always modern and relevant.</p>
-                <p> If you need a web developer who focuses on quality and efficiency, don't hesitate to get in touch with me! I'm always willing to work on new projects and excited to see how I can help you achieve your online goals.</p>
+                <p>{t('first_paragraph')}</p>
+                <p>{t('second_paragraph')}</p>
+                <p>{t('third_paragraph')}</p>
+                <p>{t('fourth_paragraph')}</p>
             </div>
         </section>
     )
