5569a883d7d2a8c530f2072463c063e41ea868ac
Lorenzo Caldera <115328112+LorenzoCaldera@users.noreply.github.com>
2023-04-28 15:50:19 -0300

    Update footer.jsx

--- src/components/footer.jsx
+++ src/components/footer.jsx
@@ -12,7 +12,12 @@ function Footer() {
     <section id="footer">
         <div className="footer__bottom d-flex align-center">
             <p>&copy; 2023 Lorenzo Caldera</p>
-            <button onClick={handleScrollTop}><span>GO UP</span></button>
+            <button onClick={handleScrollTop}>
+              <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512" height={20}>
+                {/* {Font Awesome Pro 6.4.0 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license (Commercial License) Copyright 2023 Fonticons, Inc.} */}
+                <path d="M246.6 41.4c-12.5-12.5-32.8-12.5-45.3 0l-160 160c-12.5 12.5-12.5 32.8 0 45.3s32.8 12.5 45.3 0L224 109.3 361.4 246.6c12.5 12.5 32.8 12.5 45.3 0s12.5-32.8 0-45.3l-160-160zm160 352l-160-160c-12.5-12.5-32.8-12.5-45.3 0l-160 160c-12.5 12.5-12.5 32.8 0 45.3s32.8 12.5 45.3 0L224 301.3 361.4 438.6c12.5 12.5 32.8 12.5 45.3 0s12.5-32.8 0-45.3z"/>
+              </svg>
+            </button>
         </div>
     </section>
   )
