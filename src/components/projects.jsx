8d118531e83fbac83d9364947daa23f1fc0f0161
Nahuel Ibarra <92542799+Animator55@users.noreply.github.com>
2025-03-15 13:29:53 -0300

    added project list

--- src/components/projects.jsx
+++ src/components/projects.jsx
@@ -2,41 +2,51 @@ import React from "react"
 import { useTranslation } from "react-i18next"
 
 import scrollEff from "../assets/scroll-eff"
-import ToDoList from "../images/to-do-list.png"
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
+import { faCircle } from "@fortawesome/free-solid-svg-icons"
+import projects from "../data/projectList"
+// import ToDoList from "../images/to-do-list.png"
 
-function Projects () {
-    const [projectsClass, setProjectsClass] = React.useState("show")
+
+function Projects() {
+    const [projectsClass, setProjectsClass] = React.useState("hide")
     const projectsRef = React.useRef()
 
     const { t } = useTranslation()
 
-    scrollEff([projectsRef], [setProjectsClass])
+    // scrollEff([projectsRef], [setProjectsClass])
     return (
         <section id="projects" ref={projectsRef} className={projectsClass}>
             <h1>PROJECTS</h1>
-            <div className="d-flex">
-                <div>
-                    <h2>TO-DO LIST: </h2>
-                    <p>{t('todo_list')}</p>
-                    <div className="projects__buttons">
-                        <button disabled className="visit-btn btn__hover-eff">See live</button>
-                        <a target="_blank" href="https://github.com/LorenzoCaldera/Task-List">
-                            <button className="code-btn btn__color-eff">
-                                <span className="colors-layer">
-                                    <span className="first-layer" />
-                                    <span className="second-layer" />
-                                    <span className="third-layer" />
-                                </span>
-                                <span className="text-move">
-                                    <div className="main-text">Source code</div>
-                                    <div className="secon-text">Source code</div>
-                                </span>
+            <ul className="project-list">
+                {projects.map(el => {
+                    return <li key={Math.random()}>
+                        <div>
+                            <header>
+                                <FontAwesomeIcon icon={el.icon}/>
+                                <h3>{el.name}</h3>
+                            </header>
+                            <p className="description">{el.description}</p>
+                            <div className="tags">
+                                {el.tags.map(tag => {
+                                    return <div key={Math.random()}>
+                                        <FontAwesomeIcon icon={faCircle}/>
+                                        <p>{tag}</p>
+                                    </div>
+                                })}
+                            </div>
+                        </div>
+                        <div className="projects__buttons">
+                            <button className="code-btn">
+                                <a href={el.links.github}>GitHub</a>
                             </button>
-                        </a>
-                    </div>
-                </div>
-                <img src={ToDoList} />
-            </div>
+                            <button className="visit-btn">
+                                <a href={el.links.url}>View</a>
+                            </button>
+                        </div>
+                    </li>
+                })}
+            </ul>
         </section>
     )
 }
